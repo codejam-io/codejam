43da10dc7cab5b4e1f7d2530210aedccfc26a7b7
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-04-16 17:26:29 -0700

    wip: basic html template of landing page set up

--- html/src/routes/+page.svelte
+++ html/src/routes/+page.svelte
@@ -1,5 +1,55 @@
 <script lang="ts">
-    import HomePage from "./HomePage.svelte";
-</script>
+    import '../styles/styles.css';
+    import Nav from "../lib/components/Nav.svelte";
+    import LoginBtn from "../lib/components/LoginBtn.svelte";
+    import Banner from "../lib/components/Banner.svelte";
+    import { activeContent } from '../lib/stores';
 
-<HomePage/>
+    let src = '/toast1.png'
+    let goals = "the goals of the <b>codejam</b> include..."
+    let timeline = "codejam will begin in either July or August"
+
+</script>
+<style>
+    .content {
+        padding: 3rem 5rem;
+    }
+    .panel {
+        height: 50vh;
+        border: 5px solid blue;
+        padding: 3rem 5rem;
+    }
+</style>
+<div class="flex col">
+    
+    <Nav />
+    <Banner />
+
+    <div class="flex row sp-around">
+        <LoginBtn />
+    </div>
+
+    <div class="flex panel" >
+        <div class="panel-text">
+            Ingredients for Codejam:
+            <ul>
+                <li>
+                    sugar
+                </li>
+                <li>
+                    code
+                </li>
+            </ul>
+        </div>
+        <img {src} alt="toast with jam and code on it">
+    </div>
+
+    <div class="content">
+        {#if $activeContent === 'goals'}
+        <div>{@html goals}</div>
+        {:else if $activeContent === 'timeline'}
+        <div>{@html timeline}</div>
+        {/if}
+    </div>
+
+</div>
