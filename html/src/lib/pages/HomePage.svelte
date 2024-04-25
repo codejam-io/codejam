51cd3ba7a029325eb970eb66dfca91c9064fdc16
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-04-24 18:41:03 -0700

    fixed homepage merge

--- html/src/lib/pages/HomePage.svelte
+++ html/src/lib/pages/HomePage.svelte
@@ -1,27 +1,32 @@
 <script lang="ts">
-    import {Button} from "flowbite-svelte";
-    import DiscordIcon from "../components/DiscordIcon.svelte";
-    import {baseApiUrl, logout} from "../services/services";
-    import {userStore} from "../stores/stores";
-
-    let loggedIn : boolean = false;
-
-    userStore.subscribe((value) => {
-        loggedIn = value != null;
-    });
-
-    function login() {
-        window.location = baseApiUrl +  '/oauth/redirect';
-    }
-
-	import '../../styles/styles.css';
+    import '../../styles/styles.css';
 	import Nav from '../components/Nav.svelte';
 	import Banner from '../components/Banner.svelte';
 	import GradientChange from '../components/GradientChange.svelte';
 	import Card from '../components/Card.svelte';
-	import { activeContent, loggedIn } from '../stores';
+	import { activeContent } from '../stores/stores';
 	import Panel from '../components/Panel.svelte';
 	import Content from '../components/Content.svelte';
+    import {Button} from "flowbite-svelte";
+    import DiscordIcon from "../components/DiscordIcon.svelte";
+    import {baseApiUrl, logout} from "../services/services";
+    import {userStore} from "../stores/stores";
+	import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+    
+    let loggedIn : boolean = false;
+    
+    userStore.subscribe((value) => {
+        // if value is not null, set loggedIn to True
+        // if value is null (default), set loggedIn to False
+        loggedIn = value != null
+        }
+    )
+
+    function login() {
+        if (typeof window !== 'undefined') {
+            window.location.assign(baseApiUrl +  '/oauth/redirect');
+        }
+    }
 </script>
 
 
@@ -41,11 +46,24 @@
 	<div class="flex row">
 		<div id="left-side" class="flex col">
 			<Banner size="normal" />
+
+            {#if !loggedIn}
+            <Button on:click={login}>
+                <span class="mr-3">Login with Discord</span> <DiscordIcon size="2x"/>
+            </Button>
+            {:else}
+                {$userStore?.DisplayName}
+                
+                <Button on:click={logout}>
+                    Logout
+                </Button>
+            {/if}
+
 			
-			{#if !$loggedIn}
+			<!-- {#if !$loggedIn}
             <!-- dont need big aesthetic when already logged in -->
 				<Panel />
-			{/if}
+			<!-- {/if} --> 
 			<Nav />
 			<Content />
 		</div>
