3905750fb0c0fdb9be0e0211f209463b14af3e53
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-04-24 18:40:24 -0700

    added derived loggedInStore

--- html/src/lib/stores/stores.ts
+++ html/src/lib/stores/stores.ts
@@ -1,5 +1,6 @@
 
 import {writable, derived} from 'svelte/store';
 
+export const activeContent = writable('');
 export const userStore = writable(null);
 export const loggedInStore = derived(userStore, (userData) => userData != null);
