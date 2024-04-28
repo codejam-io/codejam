c30a568c5e2c3436e665b376258ec504591da183
scottserven <scott@serven.dev>
2024-04-28 14:35:57 -0400

    added convenience DB function GetRow to take a generic struct and populate it, as opposed to having to specify every struct field on the row.Scan method

--- backend/app/database/postgres.go
+++ backend/app/database/postgres.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"os"
+	"reflect"
 )
 
 var logger = logging.NewLogger(logging.Options{Name: "Database", Level: logging.INFO})
@@ -34,4 +35,34 @@ func Initialize(config config.DBConfig) {
 
 	Pool = pool
 	logger.Info("Connected to database")
+}
+
+func GetRow[T any](output *T, query string, args ...any) error {
+	conn, err := Pool.Acquire(context.Background())
+	if err != nil {
+		logger.Error("acquire conn error %v", err)
+		return err
+	}
+	defer conn.Release()
+
+	// Look at the output value passed in, and get pointers to all the individual fields.  Those will be the Scan params
+	var pointers []any
+	v := reflect.ValueOf(output).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.CanAddr() {
+			pointers = append(pointers, field.Addr().Interface())
+		}
+	}
+
+	row := conn.QueryRow(context.Background(),
+		query,
+		args...)
+	err = row.Scan(pointers...)
+	if err != nil {
+		logger.Error("Error scanning row results: %v", err)
+		return err
+	}
+
+	return nil
 }
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -10,3 +10,40 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_service_user ON users (service_name, service_user_id);
 
+CREATE TABLE IF NOT EXISTS events (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    status_id integer,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    rules TEXT NOT NULL,
+    organizer_user_id UUID NOT NULL,
+    max_teams integer default -1,
+    starts_at TIMESTAMP WITH TIME ZONE,
+    end_at TIMESTAMP WITH TIME ZONE,
+    created_on TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE('utc'))
+);
+
+CREATE TABLE IF NOT EXISTS statuses (
+    id SERIAL PRIMARY KEY,
+    code TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+INSERT INTO statuses (id, code, title, description) VALUES (1, 'PLANNING', 'Planning', 'Event is not yet live.  The organizer may edit and preview the site but normal users will just see a placeholder page')
+ON CONFLICT (id) DO UPDATE set code = EXCLUDED.code, title = EXCLUDED.title, description = EXCLUDED.description;
+
+INSERT INTO statuses (id, code, title, description) VALUES (2, 'PUBLISHED', 'Published', 'Event is publicly visible.')
+ON CONFLICT (id) DO UPDATE set code = EXCLUDED.code, title = EXCLUDED.title, description = EXCLUDED.description;
+
+INSERT INTO statuses (id, code, title, description) VALUES (3, 'STARTED', 'Started', 'The theme will be shown publicly and users can start coding. Submissions will be accepted during this time.')
+ON CONFLICT (id) DO UPDATE set code = EXCLUDED.code, title = EXCLUDED.title, description = EXCLUDED.description;
+
+INSERT INTO statuses (id, code, title, description) VALUES (4, 'VOTING', 'Voting Open', 'Submissions are closed and voting is underway')
+ON CONFLICT (id) DO UPDATE set code = EXCLUDED.code, title = EXCLUDED.title, description = EXCLUDED.description;
+
+INSERT INTO statuses (id, code, title, description) VALUES (5, 'ENDED', 'Ended', 'Competition is over.  Results may still be viewed, but no more voting is allowed')
+ON CONFLICT (id) DO UPDATE set code = EXCLUDED.code, title = EXCLUDED.title, description = EXCLUDED.description;
+
+
+
