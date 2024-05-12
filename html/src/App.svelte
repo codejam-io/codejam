089847e46e016983b5ebb276f077073ec9a9f783
scottserven <scott@serven.dev>
2024-05-12 02:46:31 -0400

    added toast component

--- html/src/App.svelte
+++ html/src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 import routes from "./routes";
 import Router from "svelte-spa-router";
+import {Toaster} from "svelte-sonner";
 </script>
 <svelte:head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Liu+Jian+Mao+Cao&family=Space+Grotesk:wght@300..700&family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
 </svelte:head>
+
+<Toaster richColors/>
 
 <main>
     <Router {routes}/>
