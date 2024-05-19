bde5437245aa339d02fbc2e0ea43b91c2317e91a
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-05-19 01:14:12 -0700

    team schema added

--- backend/app/database/migrations/00000003_teams.up.sql
+++ backend/app/database/migrations/00000003_teams.up.sql
@@ -1,1 +1,12 @@
-CREATE TABLE IF NOT EXISTS teams
+CREATE TABLE IF NOT EXISTS teams (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    event_id UUID references events(id),
+    owner_user_id UUID references users(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    name TEXT NOT NULL,
+    visibility TEXT,
+    timezone TEXT,
+    technologies TEXT,
+    availability TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_on TIMESTAMP WITH TIME ZONE DEFAULT (now() AT TIME ZONE('utc'))
+);
